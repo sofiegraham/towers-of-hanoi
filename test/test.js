2a2c54a58d9dc7c2e1d530225f9e396c3773d5ff
sofiegraham <sofiegraham@hotmail.com>
2017-08-13 11:34:12 -0700

    Added move validation and tests

--- test/test.js
+++ test/test.js
@@ -20,37 +20,98 @@ describe('Disc', function() {
 });
 
 describe('Board', function() {
-  let board = new Board();
+  let newBoard;
 
-  const isEmpty = (item) => {
-    return item === undefined;
-  }
-
-  const isDisc = (item) => {
-    return item instanceof Disc;
-  }
-
-  it('is instantiated with three stacks', function() {
-    assert(board.stack1 !== undefined);
-    assert(board.stack2 !== undefined);
-    assert(board.stack3 !== undefined);
-    assert(board.stack4 === undefined);
+  beforeEach(function() {
+    newBoard = new Board();
   });
 
-  it('has a stack of increasing discs on stack1', function() {
-    assert(assertAll(board.stack1, isDisc) === true);
-    assert(board.stack1[0].size === 6);
-    assert(board.stack1[1].size === 5);
-    assert(board.stack1[2].size === 4);
-    assert(board.stack1[3].size === 3);
-    assert(board.stack1[4].size === 2);
-    assert(board.stack1[5].size === 1);
+  describe('constructor()', function() {
+
+    it('is instantiated with three stacks', function() {
+      assert(newBoard.stacks[0] !== undefined);
+      assert(newBoard.stacks[1] !== undefined);
+      assert(newBoard.stacks[2] !== undefined);
+      assert(newBoard.stacks[3] === undefined);
+    });
+
+    it('has a stack of increasing discs on stacks[0]', function() {
+      const isDisc = (item) => {
+        return item instanceof Disc;
+      }
+
+      assert(assertAll(newBoard.stacks[0], isDisc) === true);
+      assert(newBoard.stacks[0][0].size === 6);
+      assert(newBoard.stacks[0][1].size === 5);
+      assert(newBoard.stacks[0][2].size === 4);
+      assert(newBoard.stacks[0][3].size === 3);
+      assert(newBoard.stacks[0][4].size === 2);
+      assert(newBoard.stacks[0][5].size === 1);
+    });
+
+    it('has two empty stacks on stacks[1] and stacks[2]', function() {
+      const isEmpty = (item) => {
+        return item === undefined;
+      }
+      assert(assertAll(newBoard.stacks[1], isEmpty) === true);
+      assert(assertAll(newBoard.stacks[2], isEmpty) === true);
+    });
+
   });
 
-  it('has two empty stacks on stack2 and stack3', function() {
-    assert(assertAll(board.stack2, isEmpty) === true);
-    assert(assertAll(board.stack3, isEmpty) === true);
+  describe('isValidMove()', function() {
+
+    it('allows a disc to be moved to an empty stack', function() {
+      assert(newBoard.isValidMove(1, 2) === true);
+      assert(newBoard.isValidMove(1, 3) === true);
+    });
+
+    it('does not allow a disc to move from and to the same stack', function() {
+      assert(newBoard.isValidMove(1, 1) === false);
+    });
+
+    it('does not allow moving from an empty stack', function() {
+      assert(newBoard.isValidMove(2, 3) === false);
+    });
+
+    it('does not allow moving to or from non existent stacks', function() {
+      assert(newBoard.isValidMove(2, 4) === false);
+      assert(newBoard.isValidMove(4, 1) === false);
+    });
+
+    it('does not allow a larger disc to be placed on a smaller disc', function() {
+      newBoard.stacks[1][0] = new Disc(1);
+      newBoard.stacks[0][5] = undefined;
+      assert(newBoard.isValidMove(1, 2) === false);
+    });
+
+    it('allows a smaller disc to be placed on top of a larger disc', function() {
+      newBoard.stacks[2][0] = new Disc(2);
+      assert(newBoard.isValidMove(1, 3) === true);
+    })
+
+  });
+
+  describe('move()', function() {
+
+    it('moves the disc to the correct stack', function() {
+      newBoard.move(1 , 2);
+      assert(newBoard.stacks[1][0] instanceof Disc);
+      assert(newBoard.stacks[1][0].size === 1);
+      assert(newBoard.stacks[0][5] === undefined);
+      assert(newBoard.stacks[0][4] instanceof Disc);
+      assert(newBoard.stacks[0][4].size === 2);
+    });
+
+    it('returns false if the move is invalid', function() {
+      assert(newBoard.move(2, 3) === false);
+    });
+
+
+
   })
+
+
 
 
   // describe('promptMove()' function(){
@@ -60,6 +121,7 @@ describe('Board', function() {
   //
   //   })
   // })
+
 })
 
 
