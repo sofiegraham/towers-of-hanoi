00bf82a7f75a0b44b2c4233884b3bd70fbeb7b93
sofiegraham <sofiegraham@hotmail.com>
2017-08-13 12:29:07 -0700

    Added tests for win state

--- test/test.js
+++ test/test.js
@@ -20,19 +20,29 @@ describe('Disc', function() {
 });
 
 describe('Board', function() {
-  let newBoard;
+  let testBoard;
+  let winningBoard;
 
   beforeEach(function() {
-    newBoard = new Board();
+    testBoard = new Board();
   });
+
+  const winBoard = (board, winStackIdx) => {
+    board.stacks[winStackIdx][0] = new Disc(6)
+    board.stacks[winStackIdx][1] = new Disc(5)
+    board.stacks[winStackIdx][2] = new Disc(4)
+    board.stacks[winStackIdx][3] = new Disc(3)
+    board.stacks[winStackIdx][4] = new Disc(2)
+    board.stacks[winStackIdx][5] = new Disc(1)
+  }
 
   describe('constructor()', function() {
 
     it('is instantiated with three stacks', function() {
-      assert(newBoard.stacks[0] !== undefined);
-      assert(newBoard.stacks[1] !== undefined);
-      assert(newBoard.stacks[2] !== undefined);
-      assert(newBoard.stacks[3] === undefined);
+      assert(testBoard.stacks[0] !== undefined);
+      assert(testBoard.stacks[1] !== undefined);
+      assert(testBoard.stacks[2] !== undefined);
+      assert(testBoard.stacks[3] === undefined);
     });
 
     it('has a stack of increasing discs on stacks[0]', function() {
@@ -40,21 +50,21 @@ describe('Board', function() {
         return item instanceof Disc;
       }
 
-      assert(assertAll(newBoard.stacks[0], isDisc) === true);
-      assert(newBoard.stacks[0][0].size === 6);
-      assert(newBoard.stacks[0][1].size === 5);
-      assert(newBoard.stacks[0][2].size === 4);
-      assert(newBoard.stacks[0][3].size === 3);
-      assert(newBoard.stacks[0][4].size === 2);
-      assert(newBoard.stacks[0][5].size === 1);
+      assert(assertAll(testBoard.stacks[0], isDisc) === true);
+      assert(testBoard.stacks[0][0].size === 6);
+      assert(testBoard.stacks[0][1].size === 5);
+      assert(testBoard.stacks[0][2].size === 4);
+      assert(testBoard.stacks[0][3].size === 3);
+      assert(testBoard.stacks[0][4].size === 2);
+      assert(testBoard.stacks[0][5].size === 1);
     });
 
     it('has two empty stacks on stacks[1] and stacks[2]', function() {
       const isEmpty = (item) => {
         return item === undefined;
       }
-      assert(assertAll(newBoard.stacks[1], isEmpty) === true);
-      assert(assertAll(newBoard.stacks[2], isEmpty) === true);
+      assert(assertAll(testBoard.stacks[1], isEmpty) === true);
+      assert(assertAll(testBoard.stacks[2], isEmpty) === true);
     });
 
   });
@@ -62,32 +72,32 @@ describe('Board', function() {
   describe('isValidMove()', function() {
 
     it('allows a disc to be moved to an empty stack', function() {
-      assert(newBoard.isValidMove(1, 2) === true);
-      assert(newBoard.isValidMove(1, 3) === true);
+      assert(testBoard.isValidMove(1, 2) === true);
+      assert(testBoard.isValidMove(1, 3) === true);
     });
 
     it('does not allow a disc to move from and to the same stack', function() {
-      assert(newBoard.isValidMove(1, 1) === false);
+      assert(testBoard.isValidMove(1, 1) === false);
     });
 
     it('does not allow moving from an empty stack', function() {
-      assert(newBoard.isValidMove(2, 3) === false);
+      assert(testBoard.isValidMove(2, 3) === false);
     });
 
     it('does not allow moving to or from non existent stacks', function() {
-      assert(newBoard.isValidMove(2, 4) === false);
-      assert(newBoard.isValidMove(4, 1) === false);
+      assert(testBoard.isValidMove(2, 4) === false);
+      assert(testBoard.isValidMove(4, 1) === false);
     });
 
     it('does not allow a larger disc to be placed on a smaller disc', function() {
-      newBoard.stacks[1][0] = new Disc(1);
-      newBoard.stacks[0][5] = undefined;
-      assert(newBoard.isValidMove(1, 2) === false);
+      testBoard.stacks[1][0] = new Disc(1);
+      testBoard.stacks[0][5] = undefined;
+      assert(testBoard.isValidMove(1, 2) === false);
     });
 
     it('allows a smaller disc to be placed on top of a larger disc', function() {
-      newBoard.stacks[2][0] = new Disc(2);
-      assert(newBoard.isValidMove(1, 3) === true);
+      testBoard.stacks[2][0] = new Disc(2);
+      assert(testBoard.isValidMove(1, 3) === true);
     })
 
   });
@@ -95,30 +105,51 @@ describe('Board', function() {
   describe('move()', function() {
 
     it('moves the disc to the correct stack', function() {
-      newBoard.move(1 , 2);
-      assert(newBoard.stacks[1][0] instanceof Disc);
-      assert(newBoard.stacks[1][0].size === 1);
-      assert(newBoard.stacks[0][5] === undefined);
-      assert(newBoard.stacks[0][4] instanceof Disc);
-      assert(newBoard.stacks[0][4].size === 2);
+      testBoard.move(1 , 2);
+      assert(testBoard.stacks[1][0] instanceof Disc);
+      assert(testBoard.stacks[1][0].size === 1);
+      assert(testBoard.stacks[0][5] === undefined);
+      assert(testBoard.stacks[0][4] instanceof Disc);
+      assert(testBoard.stacks[0][4].size === 2);
     });
 
     it('returns false if the move is invalid', function() {
-      assert(newBoard.move(2, 3) === false);
+      assert(testBoard.move(2, 3) === false);
     });
 
   })
 
+  describe('isWon()', function() {
+
+    it('returns false when the board is at the starting position', function() {
+      assert(testBoard.isWon() === false);
+    });
+
+    it('returns true when the board has been won', function() {
+      winBoard(testBoard, 1);
+      assert(testBoard.isWon() === true);
+      winBoard(testBoard, 2);
+      assert(testBoard.isWon() === true);
+    });
+
+    it('returns false when the game is not yet complete', function() {
+      testBoard[1][0] = new Disc(3);
+      testBoard[2][0] = new Disc(4);
+      assert(testBoard.isWon() === false);
+    });
 
 
-
-  describe('promptMove()' function(){
-
-    it('should recieve a move from the player', function() {
-      assert()
-
-    })
   })
+
+
+
+
+  // describe('promptMove()' function(){
+  //
+  //   it('should recieve a move from the player', function() {
+  //     assert(true)
+  //   })
+  // })
 
 })
 
